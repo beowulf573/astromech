347b067e974d93e49cfeb16eec6cf4b47f419e19
Eddie McCreary <software@heorot.org>
2016-12-18 19:48:08 -0600

    basic server

---
+++ scratch/aui/server/src/myarea.cc
@@ -0,0 +1,95 @@
+#include "myarea.h"
+#include <cairomm/context.h>
+#include <giomm/resource.h>
+#include <gdkmm/general.h> // set_source_pixbuf()
+#include <glibmm/fileutils.h>
+#include <iostream>
+#include <sys/mman.h>
+#include <fcntl.h>
+
+#define SHARED_FB "shared_fb"
+
+MyArea::MyArea()
+{
+    shm_fd = -1;
+    shm_buf = NULL;
+  try
+  {
+    // The fractal image has been created by the XaoS program.
+    // http://xaos.sourceforge.net
+    //m_image = Gdk::Pixbuf::create_from_resource("/image/fractal_image.png");
+    //m_image = Gdk::Pixbuf::create(Gdk::COLORSPACE_RGB, false, 8, 320, 200);
+
+    //guint8* data = m_image->get_pixels();
+    //    guint8* data = (guint8*)malloc(3*320*200);
+    int shm_fd = shm_open(SHARED_FB, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    if(shm_fd > 0) {
+        ftruncate(shm_fd, 320*200*3);
+        shm_buf = mmap(NULL, 320*200*3, PROT_READ|PROT_WRITE, MAP_SHARED, shm_fd, (off_t)0);
+        if(shm_buf == MAP_FAILED) {
+            fprintf(stderr, "mmap failed: %s\n", strerror(errno));
+            shm_buf = NULL;
+        }
+    }
+    else {
+        fprintf(stderr, "shm_open failed: %s\n", strerror(errno));
+    }
+    
+    if(shm_buf != NULL) {
+        guint8* data = (guint8*)shm_buf;
+        m_image = Gdk::Pixbuf::create_from_data(data, Gdk::COLORSPACE_RGB, false, 8, 320, 200, 320 * 3);
+
+        for(int row = 0; row < 200; row++) {
+            for(int col = 0; col < 320; col++) {
+            //*data++ = row;
+            *data++ = 0;
+            *data++ = row;
+            *data++ = row;
+            }
+        }
+    }
+    //memset(data, 128, 3 * sizeof(guint8) * 320 * 200);
+
+  }
+  catch(const Gio::ResourceError& ex)
+  {
+    //std::cerr << "ResourceError: " << ex.what() << std::endl;
+  }
+  catch(const Gdk::PixbufError& ex)
+  {
+    //std::cerr << "PixbufError: " << ex.what() << std::endl;
+  }
+
+  // Show at least a quarter of the image.
+  if (m_image)
+    set_size_request(m_image->get_width()/2, m_image->get_height()/2);
+}
+
+MyArea::~MyArea()
+{
+    fprintf(stdout, "exiting\n");
+    if(shm_buf != NULL) {
+        munmap(shm_buf, 320*200*3);
+    }
+    if(shm_fd > 0) {
+        shm_unlink(SHARED_FB);
+    }
+}
+
+bool MyArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
+{
+  if (!m_image)
+    return false;
+
+  Gtk::Allocation allocation = get_allocation();
+  const int width = allocation.get_width();
+  const int height = allocation.get_height();
+
+  // Draw the image in the middle of the drawing area, or (if the image is
+  // larger than the drawing area) draw the middle part of the image.
+  Gdk::Cairo::set_source_pixbuf(cr, m_image,
+    (width - m_image->get_width())/2, (height - m_image->get_height())/2);
+  cr->paint();
+
+  return true;
+}
