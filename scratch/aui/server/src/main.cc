ed7e5d0a7c891ad706442dfb9773dd63275666e1
Eddie McCreary <software@heorot.org>
2016-12-20 08:27:26 -0600

    title

--- scratch/aui/server/src/main.cc
+++ scratch/aui/server/src/main.cc
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
   auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
 
   Gtk::Window win;
-  win.set_title("DrawingArea");
+  win.set_title("Astromech GUI");
   win.set_default_size(300, 200);
 
   MyArea area;
